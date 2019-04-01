ed3db210bfe760970d5456e2d3d57c528954a948
Rahul Makhija <rahuls360@gmail.com>
2019-04-01 22:40:53 +0530

    add todo using react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: ["123", "45"]
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const todos = this.state.todos;
+    todos.push(e.target.task.value);
+    this.setState({ todos: todos });
+    e.target.reset();
+  };
+
   render() {
-    return <div className="App">Hi</div>;
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Enter Task" name="task" />
+          <input type="submit" />
+        </form>
+        <ul>
+          {this.state.todos.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
